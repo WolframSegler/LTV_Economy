a458cf8e33c167d8c11badcf5ebf650c17b49935
WolframSegler <wolframsegler@gmail.com>
2025-08-25 11:41:10 +0200

    fixed some stuff on the renderer

--- src/wfg_ltv_econ/util/RenderUtils.java
+++ src/wfg_ltv_econ/util/RenderUtils.java
@@ -207,6 +207,9 @@ public class RenderUtils {
             return;
         }
 
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+        GL11.glPushMatrix();
+
         sprite.setSize(w, h);
 
         GL11.glDisable(GL11.GL_ALPHA_TEST);
@@ -215,7 +218,7 @@ public class RenderUtils {
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
         GL11.glColorMask(false, false, false, true);
-        quadNoBlend(x - w, y - h, w * 2.0F, h * 2.0F, new Color(0, 0, 0, 0), 0.0F);
+        quadWithBlend(x - w, y - h, w * 2.0F, h * 2.0F, new Color(0, 0, 0, 0), 0.0F);
 
         sprite.setBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
         sprite.setAlphaMult(alphaMult * 0.75f);
@@ -242,14 +245,20 @@ public class RenderUtils {
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_ZERO, GL11.GL_SRC_ALPHA);
 
-        quadWithBlend(x - w + 1, y - h + 1, w * 2 - 2, h * 2 - 2, Color.white, alphaMult);
-        quadWithBlend(x - w + 1, y - h + 1, w * 2 - 2, h * 2 - 2, Color.white, alphaMult);
+        quadNoBlend(x - w + 1, y - h + 1, w * 2 - 2, h * 2 - 2, Color.white, alphaMult);
+        quadNoBlend(x - w + 1, y - h + 1, w * 2 - 2, h * 2 - 2, Color.white, alphaMult);
         GL11.glColorMask(true, true, true, true);
         GL11.glBlendFunc(GL11.GL_DST_ALPHA, GL11.GL_ONE_MINUS_DST_ALPHA);
-        quadWithBlend(x - w + 1, y - h + 1, w * 2 - 2, h * 2 - 2, color, alphaMult);
+        quadNoBlend(x - w + 1, y - h + 1, w * 2 - 2, h * 2 - 2, color, alphaMult);
+
+        GL11.glPopMatrix();
+        GL11.glPopAttrib();
+
+        sprite.setAlphaMult(1);
+        sprite.setNormalBlend();
     }
 
-    public static void quadNoBlend(float x, float y, float w, float h, Color color, float alphaMult) {
+    public static void quadWithBlend(float x, float y, float w, float h, Color color, float alphaMult) {
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ZERO);
@@ -267,14 +276,10 @@ public class RenderUtils {
         GL11.glEnd();
     }
 
-    public static void quadWithBlend(float x, float y, float w, float h, Color color, float alphaMult) {
+    public static void quadNoBlend(float x, float y, float w, float h, Color color, float alphaMult) {
         GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glColor4ub(
-            (byte)  color.getRed(),
-            (byte)  color.getGreen(),
-            (byte)  color.getBlue(),
-            (byte) (color.getAlpha() * alphaMult)
-        );
+        setGlColor(color, alphaMult);
+        
         GL11.glBegin(GL11.GL_QUADS);
         GL11.glVertex2f(x, y);
         GL11.glVertex2f(x, y + h);
